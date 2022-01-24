c1539d6143e0ebd34482e7dd87508de1c6a248f4
Thiene-L <qq1031828119@gmail.com>
2022-01-24 13:53:30 +0800

    Initial commit

--- 层次分析法/层次分析法.py
+++ 层次分析法/层次分析法.py
@@ -21,7 +21,7 @@ eigenvalue, eigenvector = np.linalg.eig(array)
 # 给出矩阵最大特征值
 max_eigenvalue = max(list(map(abs, eigenvalue)))
 # 计算CR
-cr = ((max_eigenvalue - n) / (n - 1)) / n_ri[1][n-1]
+cr = ((max_eigenvalue - n) / (n - 1)) / n_ri[1][n - 1]
 
 # 判断一致性是否通过
 if cr > max_cr:
